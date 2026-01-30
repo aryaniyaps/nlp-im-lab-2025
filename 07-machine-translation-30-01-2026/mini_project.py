5bcd89e1a2ea12751465ea77401c7cb25162d5e8
Aryan Iyappan <aryaniyappan2006@gmail.com>
2026-01-30 15:15:24 +0530

    add mini project

--- 07-machine-translation-30-01-2026/mini_project.py
+++ 07-machine-translation-30-01-2026/mini_project.py
@@ -6,3 +6,34 @@ The problem statement is to design and implement a translation system that accep
 the user and allows the user to choose the target language dynamically. The system should be 
 interactive, simple to use, and demonstrate practical NLP-based machine translation. 
 """
+
+from googletrans import Translator
+import asyncio
+
+supported_languages = {
+    "ta": "tamil",
+    "hi": "hindi",
+    "fr": "french",
+    "de": "german",
+    "it": "italian",
+    "ja": "japanese",
+}
+
+async def run_program():
+    while True:
+        print("Multilingual Translation System")
+        print("Supported Languages:")
+        for code, lang in supported_languages.items():
+            print(f"{code}: {lang}")
+        print("-" * 10)
+        
+        target = input("Enter target language code: ")
+        if supported_languages.get(target) is None:
+            print("Invalid target language. Try again.")
+        else:
+            text = input("Enter source text: ")
+            translator = Translator()
+            translated = await translator.translate(text, src="en", dest=target)
+            print("Translated: ", translated.text)
+
+asyncio.run(run_program())
