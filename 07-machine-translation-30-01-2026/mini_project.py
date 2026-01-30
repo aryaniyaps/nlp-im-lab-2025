704fadd5ebf40a3fa2346dd87eb3cd03cf9b8ef7
Aryan Iyappan <aryaniyappan2006@gmail.com>
2026-01-30 15:04:40 +0530

    add mini project skeleton

---
+++ 07-machine-translation-30-01-2026/mini_project.py
@@ -0,0 +1,8 @@
+"""
+Part C- mini project
+
+Design and Implementation of a User-Choice Based Multilingual Translation System: 
+The problem statement is to design and implement a translation system that accepts text from 
+the user and allows the user to choose the target language dynamically. The system should be 
+interactive, simple to use, and demonstrate practical NLP-based machine translation. 
+"""
